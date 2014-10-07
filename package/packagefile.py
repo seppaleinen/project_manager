f6eb90e6142f3c1616ea5a2c91e1df8715a6fa19
david eriksson <davideriksson@swedenmail.com>
2014-10-07 19:52:18 +0200

    traverse

--- package/packagefile.py
+++ package/packagefile.py
@@ -1,4 +1,22 @@
 #! /usr/bin/python
 
+import os
+import fnmatch
+
 def returnWord():
 	return "String"
+
+def getWorkspace():
+	workspace = os.environ['WORKSPACE']
+	return workspace
+
+def traverse():
+    for cur, _dirs, files in os.walk("."):
+        pref = ''
+        head, tail = os.path.split(cur)
+        while head:
+            pref += '---'
+            head, _tail = os.path.split(head)
+        print(pref+tail)
+        for f in files:
+            print(pref+'---'+f)
