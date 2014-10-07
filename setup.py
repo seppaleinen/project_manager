6c94f8b9daee1170680d093e5eb48af1f1379bbd
david eriksson <davideriksson@swedenmail.com>
2014-10-07 19:35:34 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
     
 setup(
     name='pyth',
     version='0.0.14',
     author='David Eriksson',
     author_email='david.eriksson@swedenmail.com',
-    packages=[ 'package' ],
+    packages=find_packages(),
     scripts=[ 'bin/binfile' ],
     url='www.google.se',
+    zip_safe=False,
     license='LICENSE.txt',
     description='Save your code snippets in the cloud.',
+    install_requires=[],
+    tests_require=[],
     #install_requires=[
     #    "args>=0.1.0",
     #    "clint>=0.3.3",
