e1b8c79e5ce1fb267e20d6f707e8f11479668fbd
david eriksson <davideriksson@swedenmail.com>
2014-10-11 22:30:41 +0200

    Menu-class and formatted printing

--- package/menu.py
+++ package/menu.py
@@ -8,25 +8,29 @@ from package.gitManager import git_pull
 from sys import exit
 
 
-def update_git_repos():
-    for GIT_REPO in find_all_git_dirs(get_workspace()):
-        branch = git_check_for_updates(GIT_REPO)
-        if branch is not None:
-            print("Branch", branch, " in ", GIT_REPO, " is outdated")
-            checkout_result = git_checkout(GIT_REPO, branch)
-            pull_result = git_pull(GIT_REPO)
+class Menu():
+    def __init__(self):
+        print('-----------------------------------------')
+        print('What would you like to do?: enter empty to exit')
+        print('2: Update, build and deploy all outdated gitrepos')
+        user_input = raw_input('')
+        if user_input == '1':
+            print('1')
+        elif user_input == '2':
+            self.update_git_repos()
         else:
-            print("Branch ", branch, " in ", GIT_REPO, "is updated")
+            exit()
 
 
-def start_menu():
-    print('-----------------------------------------')
-    print('What would you like to do?: enter empty to exit')
-    print('2: Update, build and deploy all outdated gitrepos')
-    user_input = raw_input('')
-    if user_input == '1':
-        print('1')
-    elif user_input == '2':
-        update_git_repos()
-    else:
-        exit()
+    def update_git_repos(self):
+        for GIT_REPO in find_all_git_dirs(get_workspace()):
+            branch = git_check_for_updates(GIT_REPO)
+            if branch is not None:
+                #print "%s -> %d; stdout: '%s'; stderr: '%s'" % (cmdstr, status, stdout_value, stderr_value)
+                print "Branch %s in %s is outdated" % (branch, GIT_REPO)
+                #print("Branch", branch, " in ", GIT_REPO, " is outdated")
+                checkout_result = git_checkout(GIT_REPO, branch)
+                pull_result = git_pull(GIT_REPO)
+            else:
+                print "All Branches in %s is updated" % (GIT_REPO)
+                #print("Branch ", branch, " in ", GIT_REPO, "is updated")
