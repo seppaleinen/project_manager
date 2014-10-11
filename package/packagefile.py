87d4c229bf553ae65058628fdf866b4710d3a40e
david eriksson <davideriksson@swedenmail.com>
2014-10-11 19:42:16 +0200

    cleanup

--- package/packagefile.py
+++ package/packagefile.py
@@ -3,10 +3,6 @@
 import os
 import subprocess
 
-def return_word():
-    return "String"
-
-
 def get_workspace():
     """
     Tries to get WORKSPACE env variable
@@ -15,31 +11,8 @@ def get_workspace():
     return os.getenv('WORKSPACE', "~/")
 
 
-def traverse(directory):
-    for cur, files in os.walk(directory):
-        pref = ''
-        head, tail = os.path.split(cur)
-        while head:
-            pref += '---'
-            head, _tail = os.path.split(head)
-        print(pref + tail)
-        for f in files:
-            print(pref + '---' + f)
-
-
 def input_from_user():
     """
     Asks for input from user
     """
     return input('What directory?: ')
-
-
-def execute_command(command):
-    """
-    Execute external command
-    returns result of command or None if error
-    """
-    try:
-        return subprocess.check_output(["cd", "/opt"])
-    except OSError:
-        return None
--- tests/packagefileTest.py
+++ tests/packagefileTest.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python
 
 import unittest
-from package.packagefile import return_word
 from package.packagefile import get_workspace
-from package.packagefile import execute_command
 from os.path import expanduser
 
-class doTests(unittest.TestCase):
-	def testFirst(self):
-		self.failUnless(return_word() == "String")
-	def test_get_workspace(self):
-		workspace = get_workspace()
-		home_dir = expanduser("~")
-		self.failUnless(workspace != None)
-		#def test_execute_command(self):
-		#	command = "cd /bajs"
-		#	result = execute_command(command)
-		#	self.failUnless(result != None)
 
-def main():
-	unittest.main()
+class doTests(unittest.TestCase):
+    def test_get_workspace(self):
+        workspace = get_workspace()
+        home_dir = expanduser("~")
+        self.failUnless(workspace != None)
+
 
 if __name__ == '__main__':
-	main()
+    unittest.main()
