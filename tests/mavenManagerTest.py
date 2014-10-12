f8358c3d1536153f1a4fecfe2fe2baa12a180997
david eriksson <davideriksson@swedenmail.com>
2014-10-12 21:08:54 +0200

    removed print

--- tests/mavenManagerTest.py
+++ tests/mavenManagerTest.py
@@ -11,8 +11,6 @@ project_dir = os.getcwd()
 
 class doTests(unittest.TestCase):
     def test_mavify_pom_file(self):
-        pom_file = '%s/tests/pom.xml' % (project_dir,)
-        print(pom_file)
         result = MavenManager().mavify_pom_file('%s/tests/pom.xml' % (project_dir,))
         self.failIf(result is None)
         self.failUnless(result == 'OK')
