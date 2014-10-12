12d8929932a436361f4cb6893fe09bc85d46a0e9
david eriksson <davideriksson@swedenmail.com>
2014-10-12 19:40:51 +0200

    fixed serverManager, added mavenManager and empty pom.xml for testing

--- package/serverManager.py
+++ package/serverManager.py
@@ -5,13 +5,27 @@ import subprocess
 
 
 class ServerManager:
-    def __init__(self, ps = "jboss"):
+    def __init__(self, ps="jboss"):
         self.ps = ps
 
-
     def check_jboss_status(self):
-        ps = subprocess.Popen(['ps', '-ef'], stdout=subprocess.PIPE).communicate()[0]
-        processes = ps.split("\n")
+        """
+        Check ps -ef for jboss
+        return result
+        :return String:
+        """
+        processes = subprocess.Popen(['ps', '-ef'], stdout=subprocess.PIPE).communicate()[0].split("\n")
         for process in processes:
             if self.ps in process:
-                return 'OK'
+                return process
+
+    def get_jboss_dir(self, split_delimiter='jboss.server.base.dir='):
+        """
+        Check ps -ef for split_delimiter, default jboss.server.base.dir=
+        split result to path
+        :param split_delimiter:
+        :return String:
+        """
+        process = self.check_jboss_status()
+        if process is not None:
+            return process.split(split_delimiter)[1].split(' ')[0]
--- tests/serverManagerTest.py
+++ tests/serverManagerTest.py
@@ -6,9 +6,14 @@ from package.serverManager import ServerManager
 
 
 class doTests(unittest.TestCase):
-    def test(self):
-        result = ServerManager(ps="python").check_jboss_status()
+    def test_check_ps_for_python(self):
+        result = ServerManager(ps='python').check_jboss_status()
         self.failIf(result is None)
+        self.failUnless('python setup.py test' in result)
+    def test_get_jboss_base_dir(self):
+        result = ServerManager(ps='python').get_jboss_dir(split_delimiter='python ')
+        self.failIf(result is None)
+        self.failIf(result != 'setup.py')
 
 
 if __name__ == '__main__':
