6c021e38fa62fcd1054cc6ce0dbc87a30cb713ff
david eriksson <davideriksson@swedenmail.com>
2014-10-13 22:18:30 +0200

    compilingManager

--- package/compilingManager.py
+++ package/compilingManager.py
@@ -9,8 +9,8 @@ from os.path import isfile, join
 class CompilingManager():
     def __init__(self, dir_to_compile):
         self.dir_to_compile = dir_to_compile
-        self.file_to_compile = self.__get_compiler_type()
+        self.file_to_compile = self.dir_to_compile + self.__get_compiler_type()
 
     def __get_compiler_type(self):
-        accepted_files = {'setup.py','pom.xml'}
-        return [ f for f in listdir(self.dir_to_compile) if isfile(join(self.dir_to_compile,f)) and f in accepted_files ]
+        accepted_files = {'setup.py','pom.xml','build.gradle'}
+        return [ f for f in listdir(self.dir_to_compile) if isfile(join(self.dir_to_compile,f)) and f in accepted_files ][0]
--- tests/compilingManagerTest.py
+++ tests/compilingManagerTest.py
@@ -7,12 +7,12 @@ from package.compilingManager import CompilingManager
 
 
 class doTests(unittest.TestCase):
-    def test_find_all_git_dirs(self):
+    def test_compiling_manager_init(self):
         compilingManager = CompilingManager(os.getcwd())
         dir_to_compile = compilingManager.dir_to_compile
         file_to_compile = compilingManager.file_to_compile
         self.failUnless(dir_to_compile == os.getcwd())
-        self.failUnless(file_to_compile[0] == 'setup.py')
+        self.failUnless(file_to_compile == dir_to_compile + 'setup.py')
 
 
 if __name__ == '__main__':
