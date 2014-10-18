da04235ec5c2b29aededc7f39ffed1cddf60c1d7
david eriksson <davideriksson@swedenmail.com>
2014-10-18 20:44:18 +0200

    removed package, tests, bin, setup.py

--- src/unittest/python/maven_manager_tests.py
+++ src/unittest/python/maven_manager_tests.py
@@ -11,7 +11,7 @@ project_dir = os.getcwd()
 
 class doTests(unittest.TestCase):
     def test_mavify_pom_file(self):
-        result = MavenManager().mavify_pom_file('%s/tests/pom.xml' % (project_dir,))
+        result = MavenManager().mavify_pom_file('%s/src/unittest/python/pom.xml' % (project_dir,))
         self.failIf(result is None)
         self.failUnless(result == 'OK')
 
