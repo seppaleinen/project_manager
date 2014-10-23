41140d3f4f0d85d9a937cd33717eb6e5a7d77fce
david eriksson <davideriksson@swedenmail.com>
2014-10-23 20:17:17 +0200

    added test coverage

--- src/unittest/python/menu_tests.py
+++ src/unittest/python/menu_tests.py
@@ -2,13 +2,21 @@
 
 
 from menu import Menu
+import os
 import unittest
 
 
 class doTests(unittest.TestCase):
     def test_menu_check_git_repos(self):
+        os.environ["WORKSPACE"] = os.getcwd() + "/.git"
         Menu(user_input='1', test=True)
+
+    def test_menu_update_git_repos(self):
+        os.environ["WORKSPACE"] = os.getcwd() + "/.git"
+        Menu(user_input='2', test=True)
+
     def test_menu_check_for_uncommitted_repos(self):
+        os.environ["WORKSPACE"] = os.getcwd() + "/.git"
         Menu(user_input='3', test=True)
 
 
