a992defa570cfec423303f6a08d46f4c6c7414b7
david eriksson <davideriksson@swedenmail.com>
2014-10-28 20:21:36 +0100

    merge fix

---
+++ build.py
@@ -0,0 +1,14 @@
+from pybuilder.core import init, use_plugin
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+use_plugin("python.pycharm")
+
+default_task = "publish"
+
+@init
+def initialize(project):
+	project.build_depends_on('mockito')
