3ea4691c9313580f81bfdc873ef7db2623f61c8a
david eriksson <davideriksson@swedenmail.com>
2015-08-02 17:38:44 +0200

    mocked all tests

--- tests/test_git_manager.py
+++ tests/test_git_manager.py
@@ -11,25 +11,65 @@ project_dir = os.getcwd()
 
 
 class doTests(unittest.TestCase):
-    def test_git_pull(self):
+    @mock.patch('python_dir.git_manager.Repo')
+    def test_git_pull(self, mocked):
+        mocked.return_value = mocked
         result = git_pull(project_dir)
+        mocked.assert_called_with(project_dir)
+        mocked.git._call_process.assert_called_with('pull')
         self.failUnless(result == 'OK')
-    def test_git_checkout(self):
-        branch = "master"
-        result = git_checkout(project_dir, branch)
-        self.failUnless(result == 'OK')
-    def test_git_check_for_updates(self):
-        result = git_check_for_updates(project_dir)
-        #self.failUnless(result is None)
-    def test_git_check_for_uncommitted_changes(self):
-        git_check_for_uncommitted_changes(project_dir)
+
     @mock.patch('python_dir.git_manager.Repo')
     def test_git_pull_pass_error(self, mocked):
         mocked.return_value = mocked
-        mocked.git._call_process.side_effect = GitCommandError('Error', 'Error')
+        mocked.git._call_process.side_effect = GitCommandError('Error', 'Message')
         self.assertRaises(GitCommandError, git_pull(project_dir))
         mocked.assert_called_with(project_dir)
         mocked.git._call_process.assert_called_with("pull")
+
+    @mock.patch('python_dir.git_manager.Repo')
+    def test_git_checkout(self, mocked):
+        mocked.return_value = mocked
+        branch = "master"
+        result = git_checkout(project_dir, branch)
+        mocked.assert_called_with(project_dir)
+        mocked.git._call_process.assert_called_with('checkout', branch)
+        self.failUnless(result == 'OK')
+
+    @mock.patch('python_dir.git_manager.Repo')
+    def test_git_checkout_pass_error(self, mocked):
+        mocked.return_value = mocked
+        mocked.git._call_process.side_effect = GitCommandError('Error', 'Message')
+        branch = "master"
+        self.assertRaises(GitCommandError, git_checkout(project_dir, branch))
+        mocked.assert_called_with(project_dir)
+        mocked.git._call_process.assert_called_with('checkout', branch)
+
+    @mock.patch('python_dir.git_manager.Repo')
+    def test_git_check_for_updates(self, mocked):
+        mocked.return_value = mocked
+        mocked.git._call_process.return_value = "git result \n master pushes to master (local out of date)"
+        result = git_check_for_updates(project_dir)
+        mocked.assert_called_with(project_dir)
+        mocked.git._call_process.assert_called_with('remote', 'show', 'origin')
+        self.failIf(result is [])
+        self.failUnless('master' in result)
+
+    @mock.patch('python_dir.git_manager.Repo')
+    def test_git_check_for_updates_no_result(self, mocked):
+        mocked.return_value = mocked
+        result = git_check_for_updates(project_dir)
+        mocked.assert_called_with(project_dir)
+        mocked.git._call_process.assert_called_with('remote', 'show', 'origin')
+        self.assertEqual(result, [])
+
+    @mock.patch('python_dir.git_manager.Repo')
+    def test_git_check_for_uncommitted_changes(self, mocked):
+        mocked.return_value = mocked
+        mocked.git._call_process.return_value = "Changes not staged for commit"
+        result = git_check_for_uncommitted_changes(project_dir)
+        mocked.assert_called_with(project_dir)
+        mocked.git._call_process.assert_called_with('status')    
 
 
 if __name__ == '__main__':
