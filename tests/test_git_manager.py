cbbdf0e9e29107b48fcecf58d0f5228595044fa5
david eriksson <davideriksson@swedenmail.com>
2015-08-02 16:31:52 +0200

    mocktest

--- tests/test_git_manager.py
+++ tests/test_git_manager.py
@@ -4,25 +4,33 @@
 from python_dir.git_manager import *
 import os
 import unittest
+import mock
 
 
 project_dir = os.getcwd()
 
 
 class doTests(unittest.TestCase):
-	def test_git_pull(self):
-		result = git_pull(project_dir)
-		self.failUnless(result == 'OK')
-	def test_git_checkout(self):
-		branch = "master"
-		result = git_checkout(project_dir, branch)
-		self.failUnless(result == 'OK')
-	def test_git_check_for_updates(self):
-		result = git_check_for_updates(project_dir)
-		#self.failUnless(result is None)
-	def test_git_check_for_uncommitted_changes(self):
-		git_check_for_uncommitted_changes(project_dir)
+    def test_git_pull(self):
+        result = git_pull(project_dir)
+        self.failUnless(result == 'OK')
+    def test_git_checkout(self):
+        branch = "master"
+        result = git_checkout(project_dir, branch)
+        self.failUnless(result == 'OK')
+    def test_git_check_for_updates(self):
+        result = git_check_for_updates(project_dir)
+        #self.failUnless(result is None)
+    def test_git_check_for_uncommitted_changes(self):
+        git_check_for_uncommitted_changes(project_dir)
+    @mock.patch('python_dir.git_manager.Repo')
+    def test_git_pull_pass_error(self, mocked):
+        mocked.return_value = mocked
+        mocked.git._call_process.side_effect = GitCommandError('Error', 'Error')
+        self.assertRaises(GitCommandError, git_pull(project_dir))
+        mocked.assert_called_with(project_dir)
+        mocked.git._call_process.assert_called_with("pull")
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
