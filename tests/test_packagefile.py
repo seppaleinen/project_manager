042a5bba351641c31ba82ec3bda2420682b8391f
david eriksson <davideriksson@swedenmail.com>
2015-08-02 18:25:47 +0200

    Upped testcoverage packagefile

--- tests/test_packagefile.py
+++ tests/test_packagefile.py
@@ -2,14 +2,22 @@
 
 import unittest
 from python_dir.packagefile import get_workspace
+from python_dir.packagefile import input_from_user
 from os.path import expanduser
+import mock
 
 
 class doTests(unittest.TestCase):
     def test_get_workspace(self):
         workspace = get_workspace()
         home_dir = expanduser("~")
-        self.failUnless(workspace != None)
+        self.failIf(workspace is None)
+
+    @mock.patch('__builtin__.input')
+    def test_get_input(self, mocked):
+    	mocked.return_value = 'Return'
+    	result = input_from_user()
+    	self.assertEqual('Return', result)
 
 
 if __name__ == '__main__':
